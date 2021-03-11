68b20f175ba103445ef7e4b3b68ef21e00cbafc3
Karol <karol.swieciochowski@gmail.com>
2021-03-11 11:10:30 +0100

    nothing special

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, {Component} from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  CounterView: {
+    width: 300,
+  },
+  textCenter: {
+    textAlign: 'center'
+  }
 });
+class Counter extends Component {
+  state = { count: 0};
+
+  render(props){
+    return(
+      <View style={styles.CounterView}>
+        <Text style={styles.textCenter}>Counted {this.state.count}{"\n"}</Text>
+
+        <Button
+          onPressIn={()=>{
+            this.setState({ count: this.state.count+1});
+          }}  
+          title="ADD"
+          color="chartreuse"
+        />
+      </View>
+      
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>ELO KURWA!</Text>
+      <Counter />
+    </SafeAreaView>
+  );
+}
