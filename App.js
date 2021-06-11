259a3325a190194ba1607182d0b807095635d3fe
Karol <karol.swieciochowski@gmail.com>
2021-06-11 09:29:02 +0200

    barcode + api fetch

--- App.js
+++ App.js
@@ -1,47 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  CounterView: {
-    width: 300,
-  },
-  textCenter: {
-    textAlign: 'center'
-  }
-});
-class Counter extends Component {
-  state = { count: 0};
-
-  render(props){
-    return(
-      <View style={styles.CounterView}>
-        <Text style={styles.textCenter}>Counted {this.state.count}{"\n"}</Text>
-
-        <Button
-          onPressIn={()=>{
-            this.setState({ count: this.state.count+1});
-          }}  
-          title="ADD"
-          color="chartreuse"
-        />
-      </View>
-      
-    );
-  }
-}
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log('pre fetch');
+    fetch('https://world.openfoodfacts.org/api/v0/product/'+data+'.json')
+    .then((response) => response.json())
+    .then((json) => {
+      const name = json.product.product_name;
+      const kcal = json.product.nutriments['energy-kcal_100g'];
+      alert(`Product ${name} has ${kcal} kcal in 100g`);
+    })
+
+    
+  };
+
+  const rescanning = () => {
+    setScanned(false);
+  }
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>ELO KURWA!</Text>
-      <Counter />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={rescanning} />}
+    </View>
   );
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    paddingTop: 40
+  }
 }
