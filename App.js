7b9a785093064f3e40d45059e429111bfda55425
Karol <karol.swieciochowski@gmail.com>
2021-06-11 09:49:26 +0200

    modal

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Image, Modal } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -15,16 +16,15 @@ export default function App() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    console.log('pre fetch');
+    setModalVisible(true);
     fetch('https://world.openfoodfacts.org/api/v0/product/'+data+'.json')
     .then((response) => response.json())
     .then((json) => {
+      setModalVisible(false);
       const name = json.product.product_name;
       const kcal = json.product.nutriments['energy-kcal_100g'];
       alert(`Product ${name} has ${kcal} kcal in 100g`);
     })
-
-    
   };
 
   const rescanning = () => {
@@ -40,6 +40,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+         
+        >
+          <Image source={{uri: 'https://media2.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=790b7611047b4cd289850424f1e16fcc24fa47ddf688e0bf&rid=giphy.gif'}} />
+        </Modal>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
